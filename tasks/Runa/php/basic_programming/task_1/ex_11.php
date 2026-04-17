a444869e67d7a14f30855357b209cacff348bbc6
luna-p-dev <lunlun88wao@gmail.com>
2026-04-17 10:01:35 +0900

    feat: ex_11.php and ex_11 flow chart

---
+++ tasks/Runa/php/basic_programming/task_1/ex_11.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex_11</title>
+</head>
+<body>
+    <h1>Calculate S(n) = 1 + 1.2 + 1.2.3 + … + 1.2.3….N </h1>
+    <ul>
+        <li>Sum of factorials up to n.</li>
+        <li>Use nested loops or factorial function.</li>
+        <li>Draw a flowchart for the control structure using draw.io.</li>
+    </ul>
+
+    <form method="post">
+        <input type="number" name="n" placeholder="Enter a positive number" min="0" required>
+        <button type="submit">Calculate</button>
+    </form>
+
+    <?php
+    if ($_POST['n'] !== "") {
+        $n = (int)$_POST['n'];
+        $sum = 0;
+        for ($i = 1; $i <= $n; $i++) {
+            $factorial = 1;
+            for ($j = 1; $j <= $i; $j++) {
+                $factorial *= $j;
+            }
+            $sum += $factorial;
+        }
+        echo "<h2>Total: $sum</h2>";
+    } else {
+        echo "Enter a number";
+    }
+    ?>
+</body>
+</html>
