8f9ea5f37e6c0a61b4600096c29cc2c5702f65a8
luna-p-dev <lunlun88wao@gmail.com>
2026-04-17 10:16:23 +0900

    feat: ex_13.php and ex_13 flow chart

---
+++ tasks/Runa/php/basic_programming/task_1/ex_13.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex_12</title>
+</head>
+<body>
+    <h1>Calculate S(n) = x^2 + x^4 + … + x^2n </h1>
+    <ul>
+        <li>Sum of even powers.</li>
+        <li>Use a for loop.</li>
+    </ul>
+
+    <form method="post">
+        <input type="number" name="x" placeholder="Enter the base(x)" required>
+        <input type="number" name="n" placeholder="Enter the exponent number(n)" min="0" required>
+        <button type="submit">Calculate</button>
+    </form>
+
+    <?php
+    if ($_POST['n'] !== "") {
+        $x = (int)$_POST['x'];
+        $n = (int)$_POST['n'];
+        $sum = 0;
+        for ($i = 1; $i <= $n; $i++) {
+            $sum += $x ** (2 * $i);
+        }
+        echo "<h2>Total: $sum</h2>";
+    } else {
+        echo "Enter a number";
+    }
+    ?>
+</body>
+</html>
