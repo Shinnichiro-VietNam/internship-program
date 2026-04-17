84f1ac3e880f43cd00b9d30320b174381a091db3
luna-p-dev <lunlun88wao@gmail.com>
2026-04-17 09:20:04 +0900

    feat: ex_10.php and ex_10 flow chart

---
+++ tasks/Runa/php/basic_programming/task_1/ex_10.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex_10</title>
+</head>
+<body>
+    <h1>Calculate T(x, n) = x^n </h1>
+    <ul>
+        <li>Calculate x raised to the power n.</li>
+        <li>Use a for loop (no built-in pow).
+</li>
+    </ul>
+
+    <form method="post">
+        <input type="number" name="x" placeholder="Enter the base(x)" required>
+        <input type="number" name="n" placeholder="Enter the exponent number(n)" min="0" required>
+        <button type="submit">Calculate</button>
+    </form>
+
+    <?php
+    if ($_POST['n'] !== "") {
+        $x = (int)$_POST['x'];
+        $n = (int)$_POST['n'];
+        $sum = 1;
+        for ($i = 1; $i <= $n; $i++) {
+            $sum *= $x;
+        }
+        echo "<h2>Total: $sum</h2>";
+    } else {
+        echo "Enter a number";
+    }
+    ?>
+</body>
+</html>
