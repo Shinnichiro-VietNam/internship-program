2b31b357b05b11d60fea2d45b8f72412e26b7864
luna-p-dev <lunlun88wao@gmail.com>
2026-04-30 13:57:52 +0900

    feat: ex_2.php

---
+++ tasks/Runa/php/oop/tasks_2/ex_2.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+class Candidate{
+    private int $id;
+    private string $name;
+    private int $math;
+    private int $english;
+    private int $literature;
+
+    public function __construct(){
+
+    }
+}
