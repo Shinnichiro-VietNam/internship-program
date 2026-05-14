66085f9b80448962420cf2cbf6be6a1441c987b7
luna-p-dev <lunlun88wao@gmail.com>
2026-05-14 12:12:59 +0900

    feat: add ex_4.php

---
+++ tasks/Runa/php/algorithms/task_1/ex_4.php
@@ -0,0 +1,33 @@
+<?php
+
+function margeSort(array $arr): array {
+    if (count($arr) <= 1) {
+        return $arr;
+    }
+
+    $middle = (int)(count($arr) / 2);
+    $left = array_slice($arr, 0, $middle);
+    $right = array_slice($arr, $middle);
+
+    $left = margeSort($left);
+    $right = margeSort($right);
+
+    return marge($left, $right);
+}
+
+function marge(array $left, array $right): array {
+    $result = [];
+    while (count($left) > 0 && count($right) > 0) {
+        if ($left[0] < $right[0]) {
+            $result[] = array_shift($left);
+        } else {
+            $result[] = array_shift($right);
+        }
+    }
+    return array_merge($result, $left, $right);
+}
+
+// Test
+$arr = [2,3,6,1,5,4,45,69,23,12,45,67,89,100,123,456,789,1000];
+$sorted = margeSort($arr);
+echo "Result: ".implode(', ', $sorted )."\n";
